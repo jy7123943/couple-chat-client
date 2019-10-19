12cdd70aa44e50434eab41048552fd878cc1a08e
= <jy7123943@gmail.com>
2019-10-20 04:56:57 +0900

    style: bottomTabNavigator style

--- src/screen/main/BtmTabNavigator.js
+++ src/screen/main/BtmTabNavigator.js
@@ -5,27 +5,91 @@ import Profile from './Profile';
 import ChatRoom from './ChatRoom';
 import ChatAnalysis from './ChatAnalysis';
 import ChatAnalysisResult from './ChartAnalysisResult';
+import { StyleSheet, Image } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
 
 const TabBarComponent = props => <BottomTabBar {...props} />;
 
 const Tab = createBottomTabNavigator({
-  Profile,
+  Profile: {
+    screen: Profile,
+    navigationOptions: {
+      tabBarIcon: ({ focused }) => (
+        <Image
+          source={!focused ?
+            require('../../../assets/love_icon04.png') :
+            require('../../../assets/love_icon04_on.png')
+          }
+          style={styles.tabIcon}
+        />
+      )
+    }
+  },
   ChatRoom: {
     screen: ChatRoom,
-    navigationOptions: { tabBarVisible: false }
+    navigationOptions: {
+      tabBarVisible: false,
+      tabBarIcon: ({ focused }) => (
+        <Image
+          source={require('../../../assets/love_icon01.png')}
+          style={styles.tabIcon}
+        />
+      )
+    }
   },
-  ChatAnalysis,
-  ChatAnalysisResult
+  ChatAnalysis: {
+    screen: ChatAnalysis,
+    navigationOptions: {
+      tabBarIcon: ({ focused }) => (
+        <Image
+          source={!focused ?
+            require('../../../assets/love_icon03.png') :
+            require('../../../assets/love_icon03_on.png')
+          }
+          style={styles.tabIcon}
+        />
+      )
+    }
+  },
+  ChatAnalysisResult: {
+    screen: ChatAnalysisResult,
+    navigationOptions: {
+      tabBarIcon: ({ focused }) => (
+        <Image
+          source={!focused ?
+            require('../../../assets/love_icon02.png') :
+            require('../../../assets/love_icon02_on.png')
+          }
+          style={styles.tabIcon}
+        />
+      )
+    }
+  }
 },
 {
+  tabBarOptions: {
+    showLabel: false
+  },
   tabBarComponent: props => {
     return (
       <TabBarComponent
         {...props}
-        style={{ borderTopColor: '#605F60' }}
+        style={{
+          height: 60,
+          borderTopColor: '#40406a',
+          backgroundColor: '#40406a'
+        }}
       />
     );
-  },
+  }
+});
+
+const styles = StyleSheet.create({
+  tabIcon: {
+    width: 35,
+    height: 35,
+  }
 });
 
 export default createAppContainer(Tab);
+
